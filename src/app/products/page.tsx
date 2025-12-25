81e301a5cecf03529f95452bb68e9837eeabb90e
Sayyidath Ladheedha <sayyidathladheedha06@gmail.com>
2025-12-25 14:22:28 +0530

    Product page testing

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,17 +1,3 @@
-
-import { getAllProducts } from "@/services/productService";
-import SearchBar from "@/components/SearchBar";
-
-export const metadata = {
-  title: "All Products",
-  description: "Browse all available products",
-};
-
-// Force runtime fetch instead of prerender
-export const dynamic = "force-dynamic";
-
-export default async function ProductsPage() {
-  const products = await getAllProducts();
-
-  return <SearchBar products={products} />;
+export default function ProductsPage() {
+  return <h1 style={{ padding: 40 }}>PRODUCT PAGE ROUTE WORKS</h1>;
 }
