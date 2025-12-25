fdea37a01c456c787888c62df06cb35d6b34c15b
Sayyidath Ladheedha <sayyidathladheedha06@gmail.com>
2025-12-25 14:28:12 +0530

    product page testing

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,3 +1,5 @@
+import SearchBar from "@/components/SearchBar";
+
 export default function ProductsPage() {
-  return <h1 style={{ padding: 40 }}>PRODUCT PAGE ROUTE WORKS</h1>;
+  return <SearchBar products={[]} />;
 }
