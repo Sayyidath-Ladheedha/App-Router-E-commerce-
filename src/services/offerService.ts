2117231ac5e7b66c6ba07caf7da0d531f7453143
Sayyidath Ladheedha <sayyidathladheedha06@gmail.com>
2025-12-26 16:52:57 +0530

    fetch offer products directly from FakeStore API

--- src/services/offerService.ts
+++ src/services/offerService.ts
@@ -4,17 +4,26 @@ const BASE_URL = "https://fakestoreapi.com/products";
 
 export async function getOfferProduct(): Promise<Product[]> {
   try {
-    const res = await fetch(BASE_URL, { cache: "no-store" });
-    if (!res.ok) return [];
+    const res = await fetch(BASE_URL, {
+      cache: "no-store",
+      next: { revalidate: 0 },
+    });
 
-    const data: Product[] = await res.json();
-    if (!Array.isArray(data)) return [];
+    if (!res.ok) {
+      console.error("Fetch failed:", res.status);
+      return [];
+    }
 
-    //filter offer products onder<500
-    const offers = data.filter((product) => product.price < 500);
+    const data = await res.json();
 
-    
-    return offers.slice(0, 16);
+    if (!Array.isArray(data)) {
+      console.error("Invalid data:", data);
+      return [];
+    }
+
+    return data
+      .filter((product: Product) => product.price < 500)
+      .slice(0, 16);
   } catch (error) {
     console.error("Error fetching offer products:", error);
     return [];
